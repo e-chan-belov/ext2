011416c01ceb4b1766f3bfc226c4da9997cd42dd
e-chan-belov <echanbelov@icloud.com>
2025-10-05 14:29:37 +0300

    updt: inode macros

--- library/inode.h
+++ library/inode.h
@@ -3,6 +3,38 @@
 #include "ext2_fs.h"
 #include "super_block.h"
 
+#define S_IFSOCK 0xC000  // socket
+#define S_IFLNK  0xA000  // symbolic link
+#define S_IFREG  0x8000  // regular file
+#define S_IFBLK  0x6000  // block device
+#define S_IFDIR  0x4000  // directory
+#define S_IFCHR  0x2000  // character device
+#define S_IFIFO  0x1000  // FIFO (pipe)
+
+#define S_FF 0xF000 // file format
+
+#define IS_SOCK(i_mode) (((i_mode & S_FF) & S_IFSOCK) == S_IFSOCK)
+#define IS_LNK(i_mode) (((i_mode & S_FF) & S_IFLNK) == S_IFLNK)
+#define IS_REG(i_mode) (((i_mode & S_FF) & S_IFREG) == S_IFREG)
+#define IS_BLK(i_mode) (((i_mode & S_FF) & S_IFBLK) == S_IFBLK)
+#define IS_DIR(i_mode) (((i_mode & S_FF) & S_IFDIR) == S_IFDIR)
+#define IS_CHR(i_mode) (((i_mode & S_FF) & S_IFCHR) == S_IFCHR)
+#define IS_FIFO(i_mode) (((i_mode & S_FF) & S_IFIFO) == S_IFIFO)
+
+#define S_ISUID 0x0800  // Set process User ID
+#define S_ISGID 0x0400  // Set process Group ID
+#define S_ISVTX 0x0200  // sticky bit
+
+#define S_IRUSR 0x0100  // user read
+#define S_IWUSR 0x0080  // user write
+#define S_IXUSR 0x0040  // user execute
+#define S_IRGRP 0x0020  // group read
+#define S_IWGRP 0x0010  // group write
+#define S_IXGRP 0x0008  // group execute
+#define S_IROTH 0x0004  // others read
+#define S_IWOTH 0x0002  // others write
+#define S_IXOTH 0x0001  // others execute
+
 struct inode {
     __u16 i_mode; /* File mode */
     __u16 i_uid; /* Owner Uid */
